e1cf448accf94493b5d4e25a208630b9f90edcd3
Aleksey Popov <rusness@mail.ru>
2022-11-07 10:13:11 +0300

    Signed-off-by: Aleksey Popov <rusness@mail.ru>

---
+++ todo6/main1_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main() {
+
+	int i = 1;
+	
+	while (i < 16)
+	
+	{
+		printf("i = %i \n", i);
+		i = i + 1;
+	}
+
+}
---
+++ todo6/main1_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main() {
+	
+	int i = 1;
+	
+	while (i<15)
+	{
+		if (i%2==0)
+		{
+			printf("%i \n",i);
+		}
+		i = i + 1;
+	}
+	
+}
