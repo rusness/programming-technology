aeb31f7599fbf630b5543002ed69db72a7282b6f
Aleksey Popov <rusness@mail.ru>
2022-11-15 15:34:23 +0300

    Signed-off-by: Aleksey Popov <rusness@mail.ru>

---
+++ todo10/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main() {
+
+	int cell[7];
+	
+	for (int i=0;i<7;i++)
+	{
+		scanf("%i",&cell[i]);
+	}
+	
+	for (int i=0;i<7;i++)
+	{
+		printf("%i",cell[i]);
+	}
+	
+
+}
---
+++ todo10/main1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+int main() {
+	
+	int n;
+	
+	printf("¬ведите количество элементов массива: ");
+	scanf("%i",&n);
+	
+	int cell[n];
+	
+	for (int i=0;i<n;i++)
+	{
+		scanf("%i",&cell[i]);
+	}
+	
+	for (int i=0;i<n;i++)
+	{
+		printf("%2i",cell[i]);
+	}
+	
+	
+}
