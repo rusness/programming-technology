b463d67e483f8ed57351a4c9a22da7cb98d58be6
Aleksey Popov <rusness@mail.ru>
2022-11-23 10:02:16 +0300

    Signed-off-by: Aleksey Popov <rusness@mail.ru>

---
+++ todo13/main.c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+
+int main() {
+	
+	int m[100][100];
+	
+	int r, c;
+	
+	printf("Введите количество строк: ");
+	scanf("%d",&r);
+	
+	printf("Введите количество столбцов: ");
+	scanf("%d",&c);
+	
+	for (int j=0;j<r;j++)
+	{
+		for (int i=0;i<c;i++)
+		{
+			scanf("%i",&m[j][i]);
+		}
+	}
+
+	for (int j=0;j<r;j++)
+	{
+		for (int i=0;i<c;i++)
+		{
+			printf("%2i",m[j][i]);
+		 } 
+		 
+		 printf("\n");
+	}
+	
+}
