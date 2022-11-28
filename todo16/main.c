8c6e32fd1fa41beb000b1995d16926a382d5d548
Aleksey Popov <rusness@mail.ru>
2022-11-28 11:52:57 +0300

    Signed-off-by: Aleksey Popov <rusness@mail.ru>

---
+++ todo16/main.c
@@ -0,0 +1,75 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <locale.h>
+
+/*Отсортировать массив из n x m элементов построчно: 
+каждая строка от минимального к максимальному элементу по значению. 
+*/
+
+int main() 
+{
+	setlocale(LC_ALL, "Russian");
+
+	int m[100][100];
+	int r, c;
+	
+	printf("Введите количество строк: ");
+	scanf("%d",&r);
+	
+	printf("Введите количество столбцов: ");
+	scanf("%d",&c);
+	
+	for (int i=0;i<r;i++)
+	{
+		for (int j=0;j<c;j++)
+		{
+			scanf("%d",&m[i][j]);
+		}
+	}
+
+    printf("\n\n");
+	 
+	// вывод до
+	for (int i=0;i<r;i++)
+	{
+		for (int j=0;j<c;j++)
+		{
+			printf("%3d",m[i][j]);
+		}
+		printf("\n");
+	}	
+
+	// сортировка
+	for (int i=0;i<r;i++)
+	{
+		bool flag = true;
+		
+		while (flag)
+		{
+			flag=false;
+			for (int j=0;j<c-1;j++)
+			{
+				if (m[i][j]>m[i][j+1])
+				{
+					int z = m[i][j];
+					m[i][j]=m[i][j+1];
+					m[i][j+1] = z;
+					flag = true;
+				}
+			}
+		}
+	}
+	
+	printf("\n\n\n");
+	
+	// вывод результата
+	for (int i=0;i<r;i++)
+	{
+		for (int j=0;j<c;j++)
+		{
+			printf("%3d",m[i][j]);
+		}
+		printf("\n");
+	}		
+	
+}
---
+++ todo16/main1.c
@@ -0,0 +1,67 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <stdbool.h>
+
+/*
+
+*/
+
+int main()
+ {
+	setlocale(LC_ALL, "Russian");
+	
+	int m[100][100];
+	int r, c;
+	
+	printf("Введите количество строк: ");
+	scanf("%d",&r);
+	
+	printf("Введите количество столбцов: ");
+	scanf("%d",&c);
+	
+	for (int i=0;i<r;i++)
+	{
+		for (int j=0;j<c;j++)
+		{
+			scanf("%d", &m[i][j]);
+		}
+	}
+	
+	printf("\n\n");
+	// вывод
+		for (int i=0;i<r;i++)
+	{
+		for (int j=0;j<c;j++)
+		{
+			printf("%4d", m[i][j]);
+		}
+		printf("\n");
+	}
+	
+	
+	// меняем местами 1 и последний элементы по каждой строки
+	for (int i=0;i<r;i++)
+	{
+		int flag = true;
+		while (flag)
+		{
+			int tmp = m[i][0];
+			m[i][0]=m[i][c-1];
+			m[i][c-1]=tmp;
+			flag = false;
+		}
+	}
+	
+	
+	printf("\n\n");
+	// вывод результат
+	for (int i=0;i<r;i++)
+	{
+		for (int j=0;j<c;j++)
+		{
+			printf("%4d", m[i][j]);
+		}
+		printf("\n");
+	}	
+	return 0;
+}
