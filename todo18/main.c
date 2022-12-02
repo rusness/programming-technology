993a20d8f6a61f80c996e5dfe10075168a45afd8
Aleksey Popov <rusness@mail.ru>
2022-12-02 17:38:07 +0300

    Signed-off-by: Aleksey Popov <rusness@mail.ru>

---
+++ todo18/main.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	
+	char sr[100];
+	
+	printf("Введите строку: ");
+	gets(sr);
+	
+	// удаляем первую букву	
+	for (int i=0; i<strlen(sr);i++)
+	{
+		sr[i] = sr[i+1];
+	}
+	
+	printf("Результат - ");
+	puts(sr);
+	
+	return 0;
+}
---
+++ todo18/main1.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	char sr[100];
+	
+	printf("Введите строку: ");
+	gets(sr);
+	
+	// зеркальное отображение строки через буфернуб переменную
+	char tmp[100];
+	for (int i=0;i<strlen(sr);i++)
+	{
+		tmp[i] = sr[i];				
+	}	
+	
+	// перезыписываем	
+	for (int i=strlen(tmp)-1;i>=0;i--)
+	{
+		sr[strlen(tmp)-1-i] = tmp[i];
+	}
+	
+	printf("Результат - ");
+    puts(sr);
+	
+	return 0;
+}
