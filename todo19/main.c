326ad62f0be5a6e19aa1d061e2cd1f80270a4c0a
Aleksey Popov <rusness@mail.ru>
2022-12-07 13:42:16 +0300

    Signed-off-by: Aleksey Popov <rusness@mail.ru>

---
+++ todo19/main.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+
+void fillandshow(int mas[], int n)
+{
+	for (int i=0; i<n; i++)
+	{
+		mas[i] = rand()%100;
+		printf("%3d",mas[i]);	
+	}	
+}
+
+int main()
+{
+	
+	int n  = 4;
+	int n1 = 6;
+	int a[n];
+	int b[n1];
+	
+	fillandshow(a,n);
+	printf("\n\n");
+	fillandshow(b,n1);
+	
+	return 0;
+}
+
+
+
