a5ebc6367406968dfdc3c3b5b1c9ef483e9f8ca3
Aleksey Popov <rusness@mail.ru>
2022-12-13 14:30:34 +0300

    Signed-off-by: Aleksey Popov <rusness@mail.ru>

---
+++ todo21/main.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+
+void change_smb(char *word, char smb)
+{
+	
+	word[0] = smb;
+	word[strlen(word)] = smb;
+
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	
+	char word[100];
+	
+	printf("Введите строку: ");
+	gets(word);
+	
+	char smb = '2';
+	
+	change_smb(word, smb);
+	
+	puts(word);
+	
+	return 0;
+}
